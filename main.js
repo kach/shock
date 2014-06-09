715a90193da4b5da6aa680a1c92425fe12757811
Hardmath123 <hardmath123@gmail.com>
2014-06-08 20:20:59 -0700

    Just starting out...

---
+++ main.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+var fs = require("fs"),
+    rss = require("rss"),
+    optp = require("nomnom")
+        .script("shock");
+
+optp.command("init")
+    .callback(function() {
+        console.log("Noooo.");
+    });
+
+optp.command("compile")
+    .callback(function() {
+        fs.readFile("index.json", function(err, data) {
+            if (err) {
+                throw err;
+            }
+            var index = JSON.parse(data);
+
+            // Create RSS feed
+            var feed = new rss({
+                title: index.title,
+                description: index.description,
+                site_url: index.url,
+                author: index.author,
+                language: index.lang || "en",
+            });
+
+            var items = index.posts.sort(function(d) {
+                return new Date(d.date).getTime();
+            });
+            items.forEach(function(item) {
+                feed.item({
+                    title: item.title,
+                    description: item.description,
+                    url: index.url + "/" + item.content,
+                    date: item.date,
+                    author: item.author || index.author
+                });
+            });
+
+
+            fs.writeFile("feed.xml", feed.xml("    "), function() {
+                console.log("Created RSS feed.");
+            });
+        });
+    });
+
+var opts = optp.parse();
